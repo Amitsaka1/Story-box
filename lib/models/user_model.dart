ea9abb9be79f7688caf8e4182d74f3b0257ed9e1
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-07-16 10:45:45 +0530

    Create user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  final String id;
+  final String username;
+  final DateTime createdAt;
+
+  UserModel({required this.id, required this.username, required this.createdAt});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      username: json['username'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+}
