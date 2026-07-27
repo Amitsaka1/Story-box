7375d310002d9668540191cf875957586c96a331
Amitsaka1 <Amitsaka1@users.noreply.github.com>
2026-07-27 05:31:54 +0000

    deploy: 324e68dc774bd6b12e9fd2e91d4c495c4e51e7e4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "9418775ec76f1615e16157a221e9d9f2",
+"flutter_bootstrap.js": "41b85c7e056779a2953eb0640366d10a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
